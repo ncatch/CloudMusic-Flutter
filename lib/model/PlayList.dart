075d28d7a62375fe6fb7e73447d705d8f944f911
Walker <walker_wy@meten.com>
2021-05-12 11:14:00 +0800

    feat: 歌单详情

--- lib/model/PlayList.dart
+++ lib/model/PlayList.dart
@@ -2,17 +2,53 @@
  * @Description: 
  * @Author: Walker
  * @Date: 2021-05-11 16:45:25
- * @LastEditTime: 2021-05-11 17:51:58
+ * @LastEditTime: 2021-05-12 11:06:23
  * @LastEditors: Walker
  */
+import 'package:cloudmusic_flutter/libs/config.dart';
 import 'package:cloudmusic_flutter/model/MusicInfo.dart';
 
-class PlayList {
+class PlayListModel {
   String title = "";
   String headBgUrl = "";
+  String coverImgUrl = "";
   String descript = "";
-  var creator; // 创建人
-  List<String> labels = [];
-
+  int playCount = 0;
+  var creator = {}; // 创建人
+  var tags = [];
   List<MusicInfo> musicList = [];
+  List<dynamic> subscribers = [];
+
+  PlayListModel({
+    String title = "",
+    String headBgUrl = "",
+    String coverImgUrl = play_img_url_default,
+    String descript = "",
+    int playCount = 0,
+    creator,
+  }) {
+    this.title = title;
+    this.descript = descript;
+    this.playCount = playCount;
+
+    this.headBgUrl = headBgUrl;
+    this.coverImgUrl = coverImgUrl;
+
+    this.creator = creator ?? {};
+  }
+
+  PlayListModel.fromData(Map<String, dynamic> data) {
+    this.headBgUrl = data['backgroundCoverUrl'] ?? "";
+    this.coverImgUrl = data['coverImgUrl'] ?? play_img_url_default;
+    this.descript = data['description'];
+    this.title = data['name'];
+    this.tags = data['tags'];
+    this.playCount = data['playCount'];
+
+    this.subscribers = data['subscribers'];
+    this.creator = data['creator'];
+
+    this.musicList = List<MusicInfo>.from(
+        data['tracks'].map<MusicInfo>((ele) => MusicInfo.fromData(ele)));
+  }
 }
--- lib/pages/playList.dart
+++ lib/pages/playList.dart
@@ -2,14 +2,16 @@
  * @Description: 
  * @Author: Walker
  * @Date: 2021-05-11 15:56:11
- * @LastEditTime: 2021-05-11 19:35:37
+ * @LastEditTime: 2021-05-12 11:13:11
  * @LastEditors: Walker
  */
 import 'dart:ui';
 
 import 'package:bot_toast/bot_toast.dart';
+import 'package:cloudmusic_flutter/libs/config.dart';
 import 'package:cloudmusic_flutter/model/MusicInfo.dart';
 import 'package:cloudmusic_flutter/model/PlayInfo.dart';
+import 'package:cloudmusic_flutter/model/PlayList.dart';
 import 'package:cloudmusic_flutter/services/songList.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -24,9 +26,11 @@ class PlayList extends StatefulWidget {
 }
 
 class PlayListState extends State<PlayList> {
-  double headHeight = 300;
-  List<MusicInfo> playlist = [];
-  var songInfo = {};
+  double headHeight = 300; // head 高度
+  double appBarImgOper = 0; // appbar 透明度
+  double sunkenHeight = 20; // head 凹陷高度
+
+  PlayListModel playListInfo = PlayListModel();
 
   @override
   void initState() {
@@ -35,10 +39,7 @@ class PlayListState extends State<PlayList> {
     getPlayListById(widget.songId).then((res) {
       if (res['code'] == 200) {
         this.setState(() {
-          songInfo = res['playlist'];
-          playlist = res['playlist']['tracks']
-              .map<MusicInfo>((ele) => MusicInfo.fromData(ele))
-              .toList();
+          playListInfo = PlayListModel.fromData(res['playlist']);
         });
       } else {
         BotToast.showText(text: res['msg'] ?? '网络异常');
@@ -62,7 +63,9 @@ class PlayListState extends State<PlayList> {
                   height: headHeight,
                   decoration: new BoxDecoration(
                     image: new DecorationImage(
-                      image: new NetworkImage(songInfo['coverImgUrl']),
+                      image: new NetworkImage(playListInfo.headBgUrl == ""
+                          ? play_img_url_default
+                          : playListInfo.headBgUrl),
                       fit: BoxFit.cover,
                       colorFilter: new ColorFilter.mode(
                         Colors.black54,
@@ -72,19 +75,38 @@ class PlayListState extends State<PlayList> {
                   ),
                 ),
                 Container(
-                  height: headHeight,
-                  child: new BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  height: 50,
+                  child: Positioned(
+                    width: size.width,
+                    bottom: 0,
                     child: Opacity(
-                      opacity: 0.6,
-                      child: new Container(
-                        decoration: new BoxDecoration(
-                          color: Colors.grey.shade900,
-                        ),
+                      opacity: appBarImgOper,
+                      child: Image(
+                        width: size.width,
+                        image: NetworkImage(playListInfo.headBgUrl == ""
+                            ? play_img_url_default
+                            : playListInfo.headBgUrl),
+                        fit: BoxFit.fitWidth,
                       ),
                     ),
                   ),
                 ),
+                playListInfo.headBgUrl == ""
+                    ? Container(
+                        height: headHeight,
+                        child: new BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                          child: Opacity(
+                            opacity: 0.6,
+                            child: new Container(
+                              decoration: new BoxDecoration(
+                                color: Colors.grey.shade900,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container(),
                 Container(
                   child: AppBar(
                     leading: BackButton(),
@@ -92,11 +114,63 @@ class PlayListState extends State<PlayList> {
                     backgroundColor: Colors.transparent,
                     elevation: 0,
                     title: Text(
-                      songInfo['name'] ?? '',
+                      "歌单",
                       style: TextStyle(color: Colors.white, fontSize: 14),
                     ),
                   ),
                 ),
+                Positioned(
+                  top: 50,
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Stack(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                  playListInfo.coverImgUrl,
+                                  fit: BoxFit.fill,
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              child: Container(
+                                padding: EdgeInsets.fromLTRB(6, 0, 6, 0),
+                                decoration: BoxDecoration(
+                                  color: Colors.black38,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(50)),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.play_arrow_outlined,
+                                        color: Colors.white, size: 18),
+                                    Text(
+                                      (playListInfo.playCount / 10000)
+                                              .round()
+                                              .toString() +
+                                          '万',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              height: 22,
+                              top: 4,
+                              right: 10,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
@@ -105,13 +179,13 @@ class PlayListState extends State<PlayList> {
             width: size.width,
             height: size.height - headHeight + 50,
             child: ClipPath(
-              clipper: HeadClipper(height: 20),
+              clipper: HeadClipper(height: sunkenHeight),
               child: Container(
                 decoration: new BoxDecoration(
                   color: Colors.white,
                 ),
                 child: Wrap(
-                  children: [Text('123')],
+                  children: [],
                 ),
               ),
             ),
